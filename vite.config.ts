ab3e75bfac954a609bcd24267467696f84a2fcbd
Nitin Dhingra <nitin.dhingra@indiraivf.in>
2026-04-08 23:05:27 +0530

    Fix: bind to 0.0.0.0 for Render

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Added this
     allowedHosts: ['aatmabodha1.onrender.com']
   },
   preview: {
+    host: true, // Added this
     allowedHosts: ['aatmabodha1.onrender.com']
   }
 })
